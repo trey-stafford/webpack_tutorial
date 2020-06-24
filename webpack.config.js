93c8c0ba548441f916747d699956f15f44c7fa9c
Trey Stafford <trey.stafford@nsidc.org>
2020-06-24 14:15:43 -0600

    Add CleanWebpackplugin. Cleans up `dist/` between builds.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -37,6 +38,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Output Management',
       template: path.resolve(__dirname, 'src', 'index.ejs')
