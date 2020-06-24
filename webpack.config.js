9f8dead9bfa9d060b34cfb938b9fae38b0dc9172
Trey Stafford <trey.stafford@nsidc.org>
2020-06-24 14:35:44 -0600

    Add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
   },
   devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
   module: {
     rules: [
       {
