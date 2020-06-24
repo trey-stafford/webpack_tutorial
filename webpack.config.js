70734a4e57df9d48f3453e59bed3801c9ccdac6f
Trey Stafford <trey.stafford@nsidc.org>
2020-06-24 13:55:09 -0600

    Add HtmlWebpackPlugin, ignore `dist/`
    Automatically generates an `index.html` page with all bundles under `dist/`

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -35,4 +36,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+
+  ],
 };
